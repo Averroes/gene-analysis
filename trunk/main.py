27c18f158eb0441c82de305d9fa4e3514bb7b828
whi0003@jmss.vic.edu.au <whi0003@jmss.vic.edu.au@d6150d7a-d5ba-34a8-5cdb-42eb343b38db>
2011-08-30 05:17:55 +0000

    Improved GUI and added setting handling for file locations (.ini).

--- trunk/main.py
+++ trunk/main.py
@@ -1,7 +1,10 @@
 __author__ = 'cwhi19 and mgeb1'
 
+settingsLocation = "settings.ini"
+
 from Tkinter import *
 from tkFileDialog import *
+import ConfigParser
 
 class Application:
 
@@ -9,12 +12,16 @@ class Application:
         self.font = ("Verdana", 8)
         self.fontBold = ("Verdana", 8, "bold")
 
+        # create analyser form
+
         self.analyserForm = Frame(root)
         self.analyserForm.grid(row=0, column=0, sticky=W)
+        self.analyserForm.bind("<Return>", self.analyse)
 
         Label(self.analyserForm, text="Gene Name:", font=self.font).grid(row=0, column=0, sticky=W)
         self.geneName = Entry(self.analyserForm)
         self.geneName.grid(row=0, column=1, columnspan=2, sticky=W)
+        self.geneName.focus_set()
 
         Label(self.analyserForm, text="Simplified miRNA File:", font=self.font).grid(row=1, column=0, sticky=W)
         self.miRNAFile = Entry(self.analyserForm)
@@ -26,15 +33,67 @@ class Application:
         self.TFFile.grid(row=2, column=1, sticky=W)
         Button(self.analyserForm, text="Browse", command=self.chooseTF).grid(row=2, column=2, sticky=W)
 
-        Button(self.analyserForm, text="Analyse", font=self.fontBold).grid(row=3, column=0, columnspan=3, padx=20, pady=20, ipadx=10, ipady=5, sticky=E)
+        Label(self.analyserForm, text="Output directory:", font=self.font).grid(row=3, column=0, sticky=W)
+        self.outputFolder = Entry(self.analyserForm)
+        self.outputFolder.grid(row=3, column=1, sticky=W)
+        Button(self.analyserForm, text="Browse", command=self.chooseOutput).grid(row=3, column=2, sticky=W)
+
+        Button(self.analyserForm, text="Analyse", font=self.fontBold, command=self.analyse).grid(row=4, column=0, columnspan=3, padx=20, pady=20, ipadx=10, ipady=5, sticky=E)
+
+        # create rest of application
+
+        self.status = Label(root, font=self.font)
+        self.status.grid(row=1, column=0, sticky=W)
+
+        # retrieve settings
+
+        self.settings = ConfigParser.RawConfigParser()
+        try:
+            self.settings.read(settingsLocation)
+            replaceEntryText(self.miRNAFile, self.settings.get('locations', 'miRNA'))
+            replaceEntryText(self.TFFile, self.settings.get('locations', 'TF'))
+            self.setStatus("Application started.")
+        except ConfigParser.NoSectionError:
+            replaceEntryText(self.miRNAFile, "Resources/miRNA.txt")
+            replaceEntryText(self.TFFile, "Resources/TF.txt")
+            self.setStatus("Settings file could not be found. Reverting file locations to defaults.")
+
+            self.settings.add_section('locations')
+            self.settings.set('locations', 'miRNA', 'Resources/miRNA.txt')
+            self.settings.set('locations', 'TF', 'Resources/TF.txt')
+
+            self.updateSettings()
+
+    def analyse(self, *args):
+        self.settings.set('locations', 'miRNA', self.miRNAFile.get())
+        self.settings.set('locations', 'TF', self.TFFile.get())
+        self.updateSettings()
+
+        self.setStatus("Analysing...")
+        self.analyserForm.grid_remove()
+
+    def setStatus(self, string):
+        self.status["text"] = string
+
+    def updateSettings(self):
+        with open(settingsLocation, "wb") as settingsFile:
+                self.settings.write(settingsFile)
 
     def choosemiRNA(self):
-        self.miRNAFile.delete(0, END)
-        self.miRNAFile.insert(0, askopenfilename())
+        replaceEntryText(self.miRNAFile, askopenfilename())
 
     def chooseTF(self):
-        self.TFFile.delete(0, END)
-        self.TFFile.insert(0, askopenfilename())
+        replaceEntryText(self.TFFile, askopenfilename())
+
+    def chooseOutput(self):
+        replaceEntryText(self.outputFolder, askdirectory())
+
+    def updateOutput(self):
+        replaceEntryText(self.outputFolder, "Output/" + self.geneName.get() + "/")
+
+def replaceEntryText(widget, text):
+    widget.delete(0, END)
+    widget.insert(0, text)
 
 root = Tk()
 root.title("Gene Analyser")
