9f8c8823a51e6b9bc2889964784e9843d048f211
whi0003@jmss.vic.edu.au <whi0003@jmss.vic.edu.au@d6150d7a-d5ba-34a8-5cdb-42eb343b38db>
2011-09-07 11:51:55 +0000

    Settings handling restored to main.py.

--- trunk/main.py
+++ trunk/main.py
@@ -1,7 +1,9 @@
 __author__ = 'cwhi19 and mgeb1'
 
-import sys
+import sys, ConfigParser
 from PyQt4.Qt import *
+
+settingsLocation = "settings.ini"
 
 class Application(QWidget):
     def __init__(self):
@@ -31,12 +33,34 @@ class Application(QWidget):
         self.outputBrowse = QPushButton("Browse")
         self.grid.addWidget(self.outputBrowse, 3, 2)
 
-        self.grid.addWidget(QPushButton("Add to queue"), 4, 0, 1, 3)
+        self.queueButton = QPushButton("Add to queue")
+        self.grid.addWidget(self.queueButton, 4, 0, 1, 3)
 
         self.connect(self.geneNameInput, SIGNAL('textChanged(QString)'), (lambda x: self.updateOutput(x)))
         self.connect(self.miRNABrowse, SIGNAL('clicked()'), lambda: self.chooseFile(self.miRNAFileInput))
         self.connect(self.TFBrowse, SIGNAL('clicked()'), lambda: self.chooseFile(self.TFFileInput))
         self.connect(self.outputBrowse, SIGNAL('clicked()'), lambda: self.chooseFolder(self.outputFolderInput))
+        self.connect(self.queueButton, SIGNAL('clicked()'), self.addToQueue)
+
+        self.settings = ConfigParser.RawConfigParser()
+        try:
+            self.settings.read(settingsLocation)
+            self.miRNAFileInput.setText(self.settings.get('locations', 'miRNA'))
+            self.TFFileInput.setText(self.settings.get('locations', 'TF'))
+        except ConfigParser.NoSectionError:
+            self.miRNAFileInput.setText("Resources/miRNA.txt")
+            self.TFFileInput.setText("Resources/TF.txt")
+
+            self.settings.add_section('locations')
+            self.settings.set('locations', 'miRNA', 'Resources/miRNA.txt')
+            self.settings.set('locations', 'TF', 'Resources/TF.txt')
+
+            self.updateSettings()
+
+    def addToQueue(self):
+        self.settings.set('locations', 'miRNA', self.miRNAFileInput.text())
+        self.settings.set('locations', 'TF', self.TFFileInput.text())
+        self.updateSettings()
 
     def updateOutput(self, x):
         self.outputFolderInput.setText("Output/" + x + "/")
@@ -46,6 +70,10 @@ class Application(QWidget):
 
     def chooseFolder(self, textbox):
         textbox.setText(QFileDialog.getExistingDirectory(self, "Select folder"))
+
+    def updateSettings(self):
+        with open(settingsLocation, "wb") as settingsFile:
+                self.settings.write(settingsFile)
 
 app = QApplication(sys.argv)
 
