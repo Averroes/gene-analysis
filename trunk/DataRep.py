b706cdb4446f5d9d01ee4885ae1edf4a13ec0729
geb0001@jmss.vic.edu.au <geb0001@jmss.vic.edu.au@d6150d7a-d5ba-34a8-5cdb-42eb343b38db>
2011-09-19 12:40:41 +0000

    Added a basic sort function, will need to be changed when are importing our data. Also, search function still needs to be created, removing irrelevant data from the table.

--- trunk/DataRep.py
+++ trunk/DataRep.py
@@ -9,6 +9,8 @@ class DataRep(QWidget):
         self.setWindowTitle("Gene Data")
         self.grid = QGridLayout(self)
 
+        self.processedData = []
+
         dataFile = open(geneFolder + geneName + " - Spreadsheet.csv")
         headers = dataFile.readline().replace("\n", "").split("\t")
         dataFile.close()
@@ -25,6 +27,22 @@ class DataRep(QWidget):
 
         self.dataTable = QTableWidget(5, len(headers))
         self.grid.addWidget(self.dataTable, 1, 0, 1, 4)
+        self.dataTable.setHorizontalHeaderLabels(headers)
+        self.connect(self.orderOptions,SIGNAL("currentIndexChanged(int)"),self.sortBy)
+
+    def sortBy(self):
+        #TODO: It would be handy perhaps to intake data as lists within lists. IE [[Mirna,TF,Enrich,Size],[Mirna,TF,Enrich,Size]...]
+        index = self.orderOptions.currentIndex()
+        self.processedData = sorted(self.processedData,key=lambda x: x[index],reverse=True if index > 1 else False)
+        self.dataTable.clearContents()
+        row = 0
+        for set in self.processedData:
+            column = 0
+            for data in set:
+                Item = QTableWidgetItem(data)
+                self.dataTable.setItem(row,column,Item)
+                column +=1
+            row+=1
 
     def test(self):
         print self.dataTable.currentColumn()
