1856b9dd3d6cd9d0f365e2fba127fae35edfe81e
geb0001@jmss.vic.edu.au <geb0001@jmss.vic.edu.au@d6150d7a-d5ba-34a8-5cdb-42eb343b38db>
2011-09-20 01:33:25 +0000

    Filter now completely works, speed is fast enough.

--- trunk/DataRep.py
+++ trunk/DataRep.py
@@ -12,7 +12,17 @@ class DataRep(QWidget):
         self.processedData = []
 
         dataFile = open(geneFolder + geneName + " - Spreadsheet.csv")
-        headers = dataFile.readline().replace("\n", "").split("\t")
+        headers = dataFile.readline().replace("\n", "").split(",")
+        for line in dataFile:
+            line = line.split(',')
+            newLine = []
+            for element in line:
+                try:
+                    element = float(element)
+                except ValueError:
+                    pass
+                newLine.append(element)
+            self.processedData.append(newLine)
         dataFile.close()
 
         self.grid.addWidget(QLabel("Order by:"), 0, 0)
@@ -24,29 +34,37 @@ class DataRep(QWidget):
         self.grid.addWidget(QLabel("Filter:"), 0, 2)
         self.filterInput = QLineEdit()
         self.grid.addWidget(self.filterInput, 0, 3)
+        self.connect(self.filterInput,SIGNAL("textChanged(QString)"),self.filterData)
 
-        self.dataTable = QTableWidget(5, len(headers))
+        self.dataTable = QTableWidget(len(self.processedData), len(headers))
         self.grid.addWidget(self.dataTable, 1, 0, 1, 4)
         self.dataTable.setHorizontalHeaderLabels(headers)
+
+        self.orderOptions.setCurrentIndex(3)    
         self.connect(self.orderOptions,SIGNAL("currentIndexChanged(int)"),self.sortBy)
+        self.filterData()
+
+    def filterData(self):
+        self.writeableData = []
+        currentText = str(self.filterInput.displayText())
+        for data in self.processedData:
+            if currentText in str(data):
+                self.writeableData.append(data)
+        self.sortBy()
 
     def sortBy(self):
         #TODO: It would be handy perhaps to intake data as lists within lists. IE [[Mirna,TF,Enrich,Size],[Mirna,TF,Enrich,Size]...]
         index = self.orderOptions.currentIndex()
-        self.processedData = sorted(self.processedData,key=lambda x: x[index],reverse=True if index > 1 else False)
+        self.writeableData = sorted(self.writeableData,key=lambda x: x[index],reverse=True if index > 1 else False)
         self.dataTable.clearContents()
         row = 0
-        for set in self.processedData:
+        for set in self.writeableData:
             column = 0
             for data in set:
-                Item = QTableWidgetItem(data)
+                Item = QTableWidgetItem(str(data))
                 self.dataTable.setItem(row,column,Item)
                 column +=1
             row+=1
-
-    def test(self):
-        print self.dataTable.currentColumn()
-
 
 app = QApplication(sys.argv)
 
