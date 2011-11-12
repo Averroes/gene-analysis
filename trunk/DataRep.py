ecd0c0c6e44420cfbda303f31b4488dcdedf205f
geb0001@jmss.vic.edu.au <geb0001@jmss.vic.edu.au@d6150d7a-d5ba-34a8-5cdb-42eb343b38db>
2011-11-12 04:32:56 +0000

    Gene Viewer Added, Ie double click on a table item, and it'll show you the genes for that intersection. Still cannot mask the edit function, may have to look into that...

--- trunk/DataRep.py
+++ trunk/DataRep.py
@@ -1,3 +1,5 @@
+import time
+
 __author__ = 'cwhi19 and mgeb1'
 
 import sys, os
@@ -11,24 +13,58 @@ class DataRep(QWidget):
     def __init__(self, geneFolder):
         """Initial Function, creates a data table with the data in columns,
         default is organised by the intersections Length. """
+        QWidget.__init__(self)
+        grid1 = QGridLayout(self)
+        tabs = QTabWidget(self)
+        tabs.setMovable(True)
+        grid1.addWidget(tabs,0,0)
+
 
         geneFolder += "" if geneFolder[-1] == '/' else "/" #Provides a consistency for the form of the Destination
         geneName = geneFolder.replace('\\','/')
         geneName = geneName.split('/')[-2] #Takes the last bit of string of Destination, ie The Gene Name.
 
-        QWidget.__init__(self)
         self.setWindowTitle("Gene Data - " + geneName)
-        QWidget.setMinimumSize(self,400,200) #Sets a minimum size, smaller wouldn't be any benefit.
-        self.grid = QGridLayout(self)
+        QWidget.setMinimumSize(self,500,200) #Sets a minimum size, smaller wouldn't be any benefit.
 
+
+        self.miRNATab = QWidget()
+
+        mirnaFile = open(geneFolder + geneName +  " - TopMirna\'s.txt") #Opens the Data file of the gene name.
+        mirnaHeaders = mirnaFile.readline().replace('\n','').split('\t')
+        mirnaData = []
+        for line in mirnaFile:
+            mirnaData += [line.replace('\n','').split('\t')]
+        mirnaData = sorted(mirnaData,key=lambda x: int(x[1]),reverse=True)
+        self.grid2 = QGridLayout(self.miRNATab)
+        self.mirnaTable = QTableWidget()
+        for row in range(0,len(mirnaHeaders)):
+            self.mirnaTable.insertColumn(0)
+        for column in range(0,len(mirnaData)):
+            self.mirnaTable.insertRow(0)
+        for row in range(0,len(mirnaData)):
+            for column in range(0,len(mirnaHeaders)):
+                item = QTableWidgetItem(str(mirnaData[row][column]))
+                self.mirnaTable.setItem(row,column,item)
+        self.mirnaTable.setHorizontalHeaderLabels(mirnaHeaders)
+        self.grid2.addWidget(self.mirnaTable,0,0)
+
+        self.TableDataTab = QWidget()
+
+        self.grid = QGridLayout(self.TableDataTab)
         self.processedData = [] #String for Data from File
         self.writeableData = [] #String for Filtered Data
+        self.geneData = {}
         self.filterStr = '' #Variable for String that Data has been filtered from. Is used for speeding up filter function
+        self.geneDataWindows = []
 
-        dataFile = open(geneFolder + geneName +  " - Spreadsheet.csv") #Opens the Data file of the gene name.
-        headers = dataFile.readline().replace("\n", "").split(",") #Obtains First Line of Data, which has Headers.
-        for line in dataFile: #Takes all remaining data lines
-            line = line.split(',')
+        dataFile = open(geneFolder + geneName +  " - Results.txt") #Opens the Data file of the gene name.)
+        headers = dataFile.readline().replace("\n", "").split("\t") #Obtains First Line of Data, which has Headers.
+        headers = headers[0:4]
+        for line in dataFile:
+            line = line.split('\t')
+            genes = line[-1]
+            line = line[0:4]
             newLine = []
             for element in line:
                 try:
@@ -37,6 +73,7 @@ class DataRep(QWidget):
                     pass
                 newLine.append(element)
             self.processedData.append(newLine)
+            self.geneData.update({(line[0],line[1]):genes})
         dataFile.close()
 
         self.grid.addWidget(QLabel("Order by:"), 0, 0)
@@ -65,6 +102,15 @@ class DataRep(QWidget):
         self.addAction(self.copyAction)
         self.connect(self.copyAction,SIGNAL("triggered()"),self.copyCells)
 
+        tabs.addTab(self.TableDataTab,"Intersection Data")
+        tabs.addTab(self.miRNATab,'Top 25% MiRNAs')
+
+        self.dataTable.connect(self.dataTable,SIGNAL("cellDoubleClicked(int, int)"),self.cellDoubleClickedEvent)
+        #TODO: See below - this is the connection call.
+
+#        self.dataTable.connect(self.dataTable,SIGNAL("cellChanged(int, int)"),self.sortBy) #For some reason this code causes a maximum depth/
+#    #Recursion error. Even when using filter function, recursion somehow occurs in a different part of the function. 
+
     def copyCells(self):
         """The Copy Function for the DataTable. Copies a tab-demented form of the table to the windows clipboard.
         Can be interpreted in excel, but not many other programs."""
@@ -87,16 +133,20 @@ class DataRep(QWidget):
 
     def setColumnSizes(self):
         """To resize the column sizes based on percentage values"""
-        size =  (int(str(QWidget.size(self)).split("(")[1].split(',')[0])-80)/self.dataTable.columnCount() #The 80 compensates for the scroll bar.
+        size =  (int(str(QWidget.size(self)).split("(")[1].split(',')[0])-103)/self.dataTable.columnCount() #The 80 compensates for the scroll bar.
         for head in range(0,self.dataTable.columnCount()):
             self.dataTable.setColumnWidth(head,size)
+        size2 = (int(str(QWidget.size(self)).split("(")[1].split(',')[0])-90)/self.mirnaTable.columnCount()
+        for head in range(0,self.mirnaTable.columnCount()):
+            self.mirnaTable.setColumnWidth(head,size2)
+        
 
     def filterData(self):
         """Filters results in table based on filter input."""
         currentText = str(self.filterInput.displayText())
         newData = []
-        if self.filterStr in currentText: #Tests if what is needed to be sorted is part of what has been sorted, so save time.
-            for data in self.processedData:
+        if self.filterStr in currentText and self.filterStr != '': #Tests if what is needed to be sorted is part of what has been sorted, so save time.
+            for data in self.writeableData:
                 if currentText in str(data):
                     newData.append(data)
         else: #Filters the data based on the original data.
@@ -129,3 +179,79 @@ class DataRep(QWidget):
     def resizeEvent(self, *args, **kwargs):
         """Catches the resizeEvent call of QWidget, allowing us to respecify the column sizes."""
         self.setColumnSizes()
+
+    def cellDoubleClickedEvent(self, *args, **kwargs):
+        """Should Catch Event Of QTableWidget to edit"""
+        #TODO: Make this function occur after the Signal of a double click of a cell creates an edit instance.
+        item = self.dataTable.item(args[0],args[1])
+        self.dataTable.closePersistentEditor(item)
+        geneSet = tuple(self.writeableData[args[0]][0:2])
+        geneViewer = ViewGenes(self.geneData[geneSet],geneSet)
+        self.geneDataWindows.append(geneViewer)
+        self.geneDataWindows[-1].show()
+
+class ViewGenes(QWidget):
+    def __init__(self,genes,geneSet):
+        QWidget.__init__(self)
+        self.setWindowTitle('Gene Intersections of miRNA "' + str(geneSet[0]) + '" and TF "' + str(geneSet[1])+'".')
+        self.setMaximumSize(QSize(400,500))
+        grid = QGridLayout(self)
+        grid.addWidget(QLabel('Genes:'), 0, 0)
+        self.filterInput = QLineEdit()
+        self.connect(self.filterInput,SIGNAL("textChanged(QString)"),self.filterData)
+        grid.addWidget(self.filterInput,0,1)
+        self.table = QTableWidget()
+        self.table.insertColumn(0)
+        self.table.insertRow(0)
+        self.table.setHorizontalHeaderLabels(['Genes'])
+        grid.addWidget(self.table,1,0,1,2)
+
+        self.genes = genes.replace('[','').replace(']','').replace("'",'').replace(" ","").replace("\n",'').split(',')
+        self.writeableData = self.genes
+        self.filterStr = ''
+
+        self.sort()
+
+    def resizeEvent(self, *args):
+        self.setColumnSizes()
+
+    def setColumnSizes(self):
+        size = (int(str(QWidget.size(self)).split("(")[1].split(',')[0])-80)
+        self.table.setColumnWidth(0,size)
+
+    def filterData(self):
+        """Filters results in table based on filter input."""
+        currentText = str(self.filterInput.displayText())
+        newData = []
+        if self.filterStr in currentText and self.filterStr != '': #Tests if what is needed to be sorted is part of what has been sorted, so save time.
+            for data in self.writeableData:
+                if currentText in str(data):
+                    newData.append(data)
+        else: #Filters the data based on the original data.
+            for data in self.genes:
+                if currentText in str(data):
+                    newData.append(data)
+        self.writeableData = newData
+        self.sort() #Sorts the new writable data by the Sort Option.
+        self.filterStr = currentText #Sets a variable to remember what has already been filtered.
+
+    def sort(self):
+        self.writeableData = sorted(self.writeableData)
+        self.table.clearContents()
+        if self.table.rowCount():
+            for row in range(0,self.table.rowCount()-1):
+                self.table.removeRow(0)
+        for row in range(0,len(self.writeableData)-1):
+            self.table.insertRow(0)
+        print self.table.rowCount()
+        row = 0
+        print self.writeableData
+        for gene in self.writeableData:
+            if not row:
+                print gene
+            Item = QTableWidgetItem(str(gene))
+            self.table.setItem(row,0,Item)
+            row+=1
+        
+
+        
